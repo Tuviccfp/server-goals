61f538bce3af710a096ef06475f473abf3df6e5c
Tuviccfp <victorcordeirofp@gmail.com>
2024-09-09 20:55:34 -0300

    first commit

---
+++ src/http/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app
+  .listen({ port: 3333 })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
